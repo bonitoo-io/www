b84ef62ac768b1630b5b069f9c9a59a7a7c38f1a
ivankudibal <8028110+ivankudibal@users.noreply.github.com>
2021-11-29 17:02:54 +0100

    fix: add gtm id

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ module.exports = {
   siteDescription: 'Bonitoo s.r.o. is a software engineering company.', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml',
   siteFBAppID: '', // FB Application ID for using app insights
-  googleTagManagerID: '', // GTM tracking ID.
+  googleTagManagerID: 'GTM-KP7J3ZZ', // GTM tracking ID.
   disqusShortname: 'bonitoo-io', // Disqus shortname.
   userName: 'Ivan Kudibal',
   userTwitter: 'ivankudibal',
   userLocation: 'Prague, Czech Republic',
   userDescription: 'CEO',
-  copyright: 'Copyright © Bonitoo s.r.o. 2017 - 2021. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
+  copyright: 'Copyright © Bonitoo s.r.o. 2017 - 2022. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
   themeColor: '#9DC88D', // Used for setting manifest and progress theme colors.
   backgroundColor: '#FFFFFF', // Used for setting manifest background color.
   cookieConsent: 'This website uses cookies which are used to collect anonymous information to improve your browsing experience and for analytics and metrics.', // @TODO Add GDPR Cookie Consent
