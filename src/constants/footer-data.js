8bda8565be1257bf8c0a8333f507821b52b88627
mirek.malecha@bonitoo.io <mirek.malecha@bonitoo.io>
2022-05-20 14:40:40 +0200

    fix: company address

--- src/constants/footer-data.js
+++ src/constants/footer-data.js
@@ -1,6 +1,6 @@
 const footerData = {
     contact: {
-        address: "Kytlická 758, Praha 9, 190 00 CZ",
+        address: "Na Harfe 916/9a, Praha 9, 190 00 CZ",
         email: "info@bonitoo.io",
         website: "https://www.bonitoo.io/",
     },
